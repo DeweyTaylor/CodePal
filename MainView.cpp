7e5a1442ea79738a8b0e083d920c2c7e28e1ec79
Dewey Taylor <DeweyTaylor@users.noreply.github.com>
2017-01-26 23:30:04 +0000

    Added the framework for the main view

---
+++ MainView.cpp
@@ -0,0 +1,12 @@
+// (c) 2017 Dewey Taylor
+
+#include "MainView.h"
+
+MainView::MainView (BRect frame, char *name, BWindow* mw)
+	: BView(frame, name, B_FOLLOW_ALL, B_WILL_DRAW)
+{
+}
+
+MainView::~MainView()
+{
+}
---
+++ MainView.h
@@ -0,0 +1,16 @@
+// (c) 2017 Dewey Taylor
+
+#ifndef MAINVIEW_H
+#define MAINVIEW_H
+
+#include <InterfaceKit.h>
+
+class MainView : public BView
+{
+	public:
+		MainView(BRect frame, char *name, BWindow* mw);
+		~MainView();
+	private:
+};
+
+#endif
